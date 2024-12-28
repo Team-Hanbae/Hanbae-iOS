cfbb8102890343bed38b0fd0cb14073eb52ade3a
Yunki-H <yunki.dev@gmail.com>
2024-12-28 14:46:36 +0900

    [feat] #311 MetronomeOnOffUseCase에 Publisher 추가

--- Macro/Domain/UseCase/Interface/MetronomeOnOffUseCase.swift
+++ Macro/Domain/UseCase/Interface/MetronomeOnOffUseCase.swift
@@ -5,7 +5,11 @@
 //  Created by leejina on 11/8/24.
 //
 
+import Combine
+
 protocol MetronomeOnOffUseCase {
+    var isPlayingPublisher: AnyPublisher<Bool, Never> { get }
+    
     func changeSobak()
     func play(_ tickHandler: @escaping () -> Void )
     func stop()
