13a7041c1830ee8b2b81b686dcb1e36b77040555
Yunki-H <yunki.dev@gmail.com>
2024-12-28 17:37:18 +0900

    [refactor] #313 isSobakOnPublisher 추가

--- Macro/Domain/UseCase/MetronomeOnOffImplement.swift
+++ Macro/Domain/UseCase/MetronomeOnOffImplement.swift
@@ -24,6 +24,7 @@ class MetronomeOnOffImplement {
     private var isSobakOn: Bool
     
     private var isPlayingSubject: PassthroughSubject<Bool, Never> = .init()
+    private var isSobakOnSubject: PassthroughSubject<Bool, Never> = .init()
     private var tickSubject: PassthroughSubject<Void, Never> = .init()
     private var cancelBag: Set<AnyCancellable> = []
     
@@ -67,6 +68,10 @@ class MetronomeOnOffImplement {
 extension MetronomeOnOffImplement: MetronomeOnOffUseCase {
     var isPlayingPublisher: AnyPublisher<Bool, Never> {
         self.isPlayingSubject.eraseToAnyPublisher()
+    }
+    
+    var isSobakOnPublisher: AnyPublisher<Bool, Never> {
+        self.isSobakOnSubject.eraseToAnyPublisher()
     }
     
     var tickPublisher: AnyPublisher<Void, Never> {
