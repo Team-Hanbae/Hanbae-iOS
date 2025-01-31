346d8f3c7aec52f48532fa638f7b61051bacf276
jina <l1004ga@gmail.com>
2025-01-31 17:08:12 +0900

    [Design] #375 DisplayBlinkView 신규 생성

--- Macro/Screen/MetronomeScreen/DisplayBlinkView.swift
+++ Macro/Screen/MetronomeScreen/DisplayBlinkView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct DisplayBlinkView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            Image(.flash)
+                .aspectRatio(contentMode: .fit)
+                .padding(.trailing, 6)
+            
+            Text("화면 반짝임")
+                .font(.title3)
+                .foregroundStyle(.textSecondary)
+        }
+        .padding(.vertical, 12)
+        .padding(.leading, 22)
+        .padding(.trailing, 24)
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.backgroundCard)
+        )
     }
 }
 
