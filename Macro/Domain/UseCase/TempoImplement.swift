02fd7230e5597b9c159d5909c279c703dce9bb15
Yunki-H <yunki.dev@gmail.com>
2025-02-16 12:22:26 +0900

    [refactor] #376 TapTapUseCase를 TempoUseCase에 통합

--- Macro/Domain/UseCase/TempoImplement.swift
+++ Macro/Domain/UseCase/TempoImplement.swift
@@ -5,13 +5,37 @@
 //  Created by Lee Wonsun on 10/3/24.
 //
 
+import Combine
 import Foundation
 
 class TempoImplement {
     private var jangdanRepository: JangdanRepository
     
+    /// 마지막 tap 시점으로부터 6초가 지났을 때 이벤트를 전달하기 위한 publisher
+    @Published private var lastTappedDate: Date?
+    private var isTappingSubject: PassthroughSubject<Bool, Never>
+    private var isTapping: Bool
+    private var timeStampList: [Date]
+    
+    private var cancelBag: Set<AnyCancellable>
+    
     init(jangdanRepository: JangdanRepository) {
         self.jangdanRepository = jangdanRepository
+        
+        self.isTappingSubject = .init()
+        self.isTapping = false
+        self.timeStampList = .init()
+        
+        self.cancelBag = .init()
+        
+        $lastTappedDate
+            .debounce(for: .seconds(6), scheduler: DispatchQueue.main)
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.finishTapping()
+                self.isTappingSubject.send(self.isTapping)
+            }
+            .store(in: &cancelBag)
     }
 }
 
@@ -28,5 +52,36 @@ extension TempoImplement: TempoUseCase {
         default :
             break
         }
+    }
+    
+    var isTappingPublisher: AnyPublisher<Bool, Never> {
+        self.isTappingSubject.eraseToAnyPublisher()
+    }
+    
+    func tap(timeStamp: Date) {
+        if !isTapping {
+            isTapping = true
+            self.isTappingSubject.send(self.isTapping)
+        }
+        
+        lastTappedDate = timeStamp
+        self.timeStampList.append(timeStamp)
+        
+        if timeStampList.count > 5 {
+            self.timeStampList.removeFirst()
+        }
+        
+        guard timeStampList.count > 1 else { return }
+        
+        let interval: TimeInterval = timeStampList.last!.timeIntervalSince(timeStampList.first!)
+        let averageInterval: TimeInterval = interval / Double(timeStampList.count - 1)
+        let tempo: Double = 60 / averageInterval
+        self.updateTempo(newBpm: Int(tempo.rounded()))
+    }
+    
+    func finishTapping() {
+        self.isTapping = false
+        self.isTappingSubject.send(self.isTapping)
+        self.timeStampList.removeAll()
     }
 }
