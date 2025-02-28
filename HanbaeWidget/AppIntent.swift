16b4789c819b40201799fafe56c88f9532f4a35c
Yunki-H <yunki.dev@gmail.com>
2025-02-28 19:59:43 +0900

    [feat] #381 주석 삭제

--- HanbaeWidget/AppIntent.swift
+++ HanbaeWidget/AppIntent.swift
@@ -5,19 +5,7 @@
 //  Created by leejina on 2/16/25.
 //
 
-import WidgetKit
 import AppIntents
-import ActivityKit
-import Combine
-
-//struct ConfigurationAppIntent: WidgetConfigurationIntent {
-//    static var title: LocalizedStringResource { "Configuration" }
-//    static var description: IntentDescription { "This is an example widget." }
-//
-//    // An example configurable parameter.
-//    @Parameter(title: "Favorite Emoji", default: "😃")
-//    var favoriteEmoji: String
-//}
 
 struct TogglePlayingIntent: LiveActivityIntent {
     static var title: LocalizedStringResource = "Toggle Play/Pause"
@@ -30,54 +18,3 @@ struct TogglePlayingIntent: LiveActivityIntent {
         return .result()
     }
 }
-
-//@Observable
-//class WidgetViewModel {
-//    private var templateUseCase: TemplateUseCase
-//    private var metronomeOnOffUseCase: MetronomeOnOffUseCase
-//    
-//    private var cancelBag: Set<AnyCancellable> = []
-//    
-//    init(templateUseCase: TemplateUseCase, metronomeOnOffUseCase: MetronomeOnOffUseCase) {
-//        
-//        self.templateUseCase = templateUseCase
-//        self.metronomeOnOffUseCase = metronomeOnOffUseCase
-//        
-//        self.templateUseCase.currentJangdanTypePublisher.sink { [weak self] jangdanType in
-//            guard let self else { return }
-//            self.state.currentJangdanType = jangdanType
-//        }
-//        .store(in: &self.cancelBag)
-//        
-//        self.metronomeOnOffUseCase.isPlayingPublisher.sink { [weak self] isPlaying in
-//            guard let self else { return }
-//            self.state.isPlaying = isPlaying
-//        }
-//        .store(in: &self.cancelBag)
-//    }
-//    
-//    private(set) var state: State = .init()
-//    
-//    struct State {
-//        var currentJangdanName: String?
-//        var currentJangdanType: Jangdan?
-//        var isPlaying: Bool = false
-//        var bpm: Int = 60
-//    }
-//}
-//
-//extension WidgetViewModel {
-//    enum Action: Equatable {
-//        case updateBPM
-//        case updateJangdanName
-//    }
-//    
-//    func effect(action: Action) {
-//        switch action {
-//        case .updateBPM:
-//            self.
-//        case .updateJangdanName:
-//            
-//        }
-//    }
-//}
