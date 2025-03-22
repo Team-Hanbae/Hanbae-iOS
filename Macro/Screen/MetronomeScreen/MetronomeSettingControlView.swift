15b836b6dd3342f3448f37f12506ba5172e1472e
Yunki-H <yunki.dev@gmail.com>
2025-03-22 18:32:15 +0900

    [feat] #400 MetronomeSettingView 변경

--- Macro/Screen/MetronomeScreen/MetronomeSettingControlView.swift
+++ Macro/Screen/MetronomeScreen/MetronomeSettingControlView.swift
@@ -8,64 +8,82 @@
 import SwiftUI
 
 struct MetronomeSettingControlView: View {
-    
     @State private var viewModel: MetronomeViewModel = DIContainer.shared.metronomeViewModel
-    @State private var isSobakOn: Bool = false
-    @State private var isBlinkOn: Bool = false
     
     var body: some View {
         HStack(spacing: 8) {
             Button {
-                self.isSobakOn.toggle()
                 self.viewModel.effect(action: .changeSobakOnOff)
             } label: {
-                HStack(spacing: 6) {
-                    Image(self.viewModel.state.currentJangdanType?.sobakSegmentCount == nil ? .listenSobak : .viewSobak)
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundStyle(self.isSobakOn ? Color.buttonToggleOn : Color.textSecondary)
-                    
-                    Text(self.viewModel.state.currentJangdanType?.sobakSegmentCount == nil ? "소박 듣기" : "소박 보기")
-                        .font(.title3)
-                        .foregroundStyle(self.isSobakOn ? Color.buttonToggleOn : Color.textSecondary)
-                }
-                .padding(.vertical, 12)
-                .padding(.leading, 22)
-                .padding(.trailing, 24)
-                .frame(maxWidth: .infinity)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(self.isSobakOn ? Color.backgroundCard : Color.buttonToggleOff)
-                        .strokeBorder(self.isSobakOn ? Color.buttonToggleOn : Color.buttonToggleOff)
-                )
+                Image(self.viewModel.state.currentJangdanType?.sobakSegmentCount == nil ? .listenSobak : .viewSobak)
+                    .aspectRatio(contentMode: .fit)
             }
+            .buttonStyle(MetronomeSettingToggleButtonStyle())
             
             Button {
-                self.isBlinkOn.toggle()
                 self.viewModel.effect(action: .changeBlinkOnOff)
             } label: {
-                HStack(spacing: 6) {
-                    Image(.flash)
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundStyle(self.isBlinkOn ? Color.buttonToggleOn : Color.textSecondary)
-                    
-                    Text("화면 반짝임")
-                        .font(.title3)
-                        .foregroundStyle(self.isBlinkOn ? Color.buttonToggleOn : Color.textSecondary)
-                }
-                .padding(.vertical, 12)
-                .padding(.leading, 22)
-                .padding(.trailing, 24)
-                .frame(maxWidth: .infinity)
-                .background(
-                    RoundedRectangle(cornerRadius: 12)
-                        .fill(self.isBlinkOn ? Color.backgroundCard : Color.buttonToggleOff)
-                        .strokeBorder(self.isBlinkOn ? Color.buttonToggleOn : Color.buttonToggleOff)
-                )
+                Image(.flash)
+                    .aspectRatio(contentMode: .fit)
             }
+            .buttonStyle(MetronomeSettingToggleButtonStyle())
+            
+            Menu {
+                Text("장구")
+                Text("북")
+                Text("나무")
+            } label: {
+                Image(systemName: "speaker.wave.2.fill")
+            }
+            .buttonStyle(MetronomeSettingMenuButtonStyle())
         }
         .frame(maxWidth: .infinity)
         .padding(.horizontal, 16)
         .padding(.bottom, 16)
+    }
+}
+
+extension MetronomeSettingControlView {
+    private struct MetronomeSettingToggleButtonStyle: ButtonStyle {
+        @State var isOn: Bool = false
+        
+        func makeBody(configuration: Configuration) -> some View {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(isOn ? Color.backgroundCard : Color.buttonToggleOff)
+                    .strokeBorder(isOn ? Color.buttonToggleOn : Color.buttonToggleOff)
+                
+                configuration.label
+                    .font(.title3)
+                    .foregroundStyle(isOn ? Color.buttonToggleOn : Color.textSecondary)
+            }
+            .frame(height: 56)
+            .onChange(of: configuration.isPressed) { _, newValue in
+                if newValue {
+                    isOn.toggle()
+                }
+            }
+        }
+    }
+    
+    private struct MetronomeSettingMenuButtonStyle: ButtonStyle {
+        @State var isActive: Bool = false
+        
+        func makeBody(configuration: Configuration) -> some View {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(isActive ? Color.backgroundCard : Color.buttonToggleOff)
+                    .strokeBorder(isActive ? Color.buttonToggleOn : Color.buttonToggleOff)
+                
+                configuration.label
+                    .font(.title3)
+                    .foregroundStyle(isActive ? Color.buttonToggleOn : Color.textSecondary)
+            }
+            .frame(height: 56)
+            .onChange(of: configuration.isPressed) { _, newValue in
+                isActive = newValue
+            }
+        }
     }
 }
 
