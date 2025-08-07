8d2be2e685dba1e3efe2cf53f2bea7e95716ae2a
Yunki-H <yunki.dev@gmail.com>
2025-08-07 13:53:04 +0900

    [fix] #474 타이머 생성에 소요되는 시간에 기인한 첫박 밀림현상 해결

--- Hanbae/Domain/UseCase/MetronomeOnOffImplement.swift
+++ Hanbae/Domain/UseCase/MetronomeOnOffImplement.swift
@@ -111,10 +111,14 @@ extension MetronomeOnOffImplement: MetronomeOnOffUseCase {
         self.currentBeatIndex = 0
         self.initialDaeSoBakIndex()
         UIApplication.shared.isIdleTimerDisabled = true
+        
+        let deadline: DispatchTime = .now() + self.interval
+        self.timerHandler()
+        
         // Timer 설정
         if self.timer != nil { self.stop() }
         self.timer = DispatchSource.makeTimerSource(queue: self.queue)
-        self.timer?.schedule(deadline: .now(), repeating: self.interval, leeway: .nanoseconds(1))
+        self.timer?.schedule(deadline: deadline, repeating: self.interval, leeway: .nanoseconds(1))
         self.timer?.setEventHandler { [weak self] in
             guard let self = self else { return }
             self.lastPlayTime = .now
@@ -123,6 +127,7 @@ extension MetronomeOnOffImplement: MetronomeOnOffUseCase {
         
         // play 여부 publish
         self.isPlayingSubject.send(true)
+        
         // Timer 실행
         self.timer?.resume()
     }
