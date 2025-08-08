f6512bbd3ee19fab0d6b50eb529b0f7b3f5a28bd
Yunki-H <yunki.dev@gmail.com>
2025-08-09 07:38:46 +0900

    [chore] #478 Xcode Cloud용 스크립트 파일 생성

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Hanbae
+#
+#  Created by Yunki on 8/9/25.
+#  
+
+ENV_FILE_PATH="${CI_PRIMARY_REPOSITORY_PATH}/env.xcconfig"
+
+echo "enf.xcconfig 파일을 생성합니다."
+
+echo "MIXPANEL_TOKEN = ${MIXPANEL_TOKEN}" > "${ENV_FILE_PATH}"
+
+echo "enf.xcconfig 파일을 생성했습니다."
