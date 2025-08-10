dc7327c970a064e7e518d04fa5cf8cc4457e12a1
Yunki-H <yunki.dev@gmail.com>
2025-08-10 10:59:12 +0900

    [feat] #479 빌드 환경에 따른 토큰 분기처리

--- Hanbae/Service/MixpanelManager.swift
+++ Hanbae/Service/MixpanelManager.swift
@@ -12,10 +12,22 @@ class MixpanelManager {
     static let shared = MixpanelManager()
     
     private init() {
-        guard let token = Bundle.main.object(forInfoDictionaryKey: "MIXPANEL_TOKEN") as? String else {
+        #if DEBUG
+        guard let token = Bundle.main.object(forInfoDictionaryKey: "MIXPANEL_DEV_TOKEN") as? String else {
             print("Mixpanel token not found in Info.plist")
             return
         }
+        #else
+        var isTestFlight: Bool {
+            guard let path = Bundle.main.appStoreReceiptURL?.path else { return false }
+            return path.contains("sandboxReceipt")
+        }
+        print("MIXPANEL_\(isTestFlight ? "DEV" : "PROD")_TOKEN")
+        guard let token = Bundle.main.object(forInfoDictionaryKey: "MIXPANEL_\(isTestFlight ? "DEV" : "PROD")_TOKEN") as? String else {
+            print("Mixpanel token not found in Info.plist")
+            return
+        }
+        #endif
         Mixpanel.initialize(token: token, trackAutomaticEvents: false)
     }
 }
