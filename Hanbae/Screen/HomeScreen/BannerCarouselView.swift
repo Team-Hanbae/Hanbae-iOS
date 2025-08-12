5a67292f75c4eda94aeb608d1ea1e3dfa511130a
Yunki-H <yunki.dev@gmail.com>
2025-08-12 18:29:18 +0900

    [feat] #482 Carousel Pagenation

--- Hanbae/Screen/HomeScreen/BannerCarouselView.swift
+++ Hanbae/Screen/HomeScreen/BannerCarouselView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BannerCarouselView: View {
     let banners: [BannerInfo]
     
-    @State private var currentPage: Int? = 0
+    @State private var currentPage: Int! = 0
     
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
@@ -31,13 +31,33 @@ struct BannerCarouselView: View {
                     }
                 }
                 .scrollTargetLayout()
-                
             }
             .scrollPosition(id: $currentPage, anchor: .leading)
             .scrollTargetBehavior(.paging)
             .scrollIndicators(.hidden)
             
-            Text("\(currentPage)")
+            HStack(spacing: 4) {
+                Text("\(currentPage + 1)")
+                    .foregroundStyle(.textDefault)
+                    .font(.Caption1_R)
+                
+                Rectangle()
+                    .frame(width: 1, height: 11)
+                    .foregroundStyle(.textTertiary)
+                
+                Text("\(banners.count)")
+                    .foregroundStyle(.textTertiary)
+                    .font(.Caption1_R)
+                
+                Image(systemName: "chevron.right")
+                    .resizable()
+                    .frame(width: 4, height: 8)
+                    .foregroundStyle(.textDefault)
+            }
+            .padding(EdgeInsets(top: 4, leading: 12, bottom: 4, trailing: 12))
+            .background(.backgroundDefault.opacity(0.6))
+            .cornerRadius(500)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 24))
         }
     }
 }
