357659c1481b5ba96e6700256b5bed0f5be0885a
Yunki-H <yunki.dev@gmail.com>
2025-08-17 00:12:44 +0900

    [feat] #484 무한스크롤인 척 하기

--- Hanbae/Screen/HomeScreen/BannerCarouselView.swift
+++ Hanbae/Screen/HomeScreen/BannerCarouselView.swift
@@ -10,28 +10,27 @@ import Combine
 
 struct BannerCarouselView: View {
     let banners: [BannerInfo]
-    let currentIndex: Int!
+    let currentIndex: Int
     let onIndexChange: (Int) -> Void
     
-    private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+    @State private var internalCurrentIndex: Int?
+    
+    @State private var timerSubscription: AnyCancellable?
+    
+    private let repeatBannerCount = 21
+    
+    private var extendedBanners: [BannerInfo] {
+        Array(repeating: banners, count: repeatBannerCount).flatMap{ $0 }
+    }
     
     var body: some View {
-        let currentIndexBinding = Binding(
-            get: { currentIndex },
-            set: { newIndex in
-                if let newIndex {
-                    onIndexChange(newIndex)
-                }
-            }
-        )
-        
         ZStack(alignment: .bottomTrailing) {
             ScrollView(.horizontal) {
                 HStack(spacing: 0) {
-                    ForEach(banners.indices, id: \.self) { index in
-                        if let surveyURL = URL(string: banners[index].urlString) {
+                    ForEach(extendedBanners.indices, id: \.self) { index in
+                        if let surveyURL = URL(string: extendedBanners[index].urlString) {
                             Link(destination: surveyURL) {
-                                Image(banners[index].imageResource)
+                                Image(extendedBanners[index].imageResource)
                                     .resizable()
                                     .scaledToFit()
                                     .clipShape(RoundedRectangle(cornerRadius: 16))
@@ -44,25 +43,48 @@ struct BannerCarouselView: View {
                 }
                 .scrollTargetLayout()
             }
-            .scrollPosition(id: currentIndexBinding, anchor: .center)
+            .scrollPosition(id: $internalCurrentIndex, anchor: .center)
             .scrollTargetBehavior(.paging)
             .scrollIndicators(.hidden)
+            .onChange(of: internalCurrentIndex) { _, newValue in
+                if let newValue {
+                    onIndexChange(newValue % banners.count)
+                }
+            }
             
             pagenation
         }
         .frame(height: 120)
-        .onReceive(timer) { _ in
-            var newIndex = currentIndex + 1
-            if newIndex >= banners.count { newIndex = 0 }
-            withAnimation {
-                onIndexChange(newIndex)
+        .onAppear {
+            var transaction = Transaction()
+            transaction.disablesAnimations = true
+            withTransaction(transaction) {
+                internalCurrentIndex = repeatBannerCount / 2 * banners.count + currentIndex
             }
+            
+            self.timerSubscription = Timer.publish(every: 5, on: .main, in: .common)
+                .autoconnect()
+                .sink { _ in
+                    withAnimation {
+                        if let index = internalCurrentIndex {
+                            if index >= extendedBanners.count - 1 {
+                                self.internalCurrentIndex = repeatBannerCount / 2 * banners.count + currentIndex
+                            } else {
+                                self.internalCurrentIndex = index + 1
+                            }
+                        }
+                    }
+                }
+        }
+        .onDisappear {
+            self.timerSubscription?.cancel()
+            self.timerSubscription = nil
         }
     }
     
     private var pagenation: some View {
         HStack(spacing: 4) {
-            Text("\(currentIndex + 1)")
+            Text("\(realCurrentIndex + 1)")
                 .foregroundStyle(.textDefault)
                 .font(.pretendardRegular(size: 12))
             
@@ -83,6 +105,11 @@ struct BannerCarouselView: View {
         .background(.backgroundDefault.opacity(0.6))
         .cornerRadius(500)
         .padding(EdgeInsets(top: 0, leading: 0, bottom: 6, trailing: 24))
+    }
+    
+    private var realCurrentIndex: Int {
+        guard let internalCurrentIndex else { return 0 }
+        return internalCurrentIndex % banners.count
     }
 }
 
