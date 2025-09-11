a0c2156c473f94f0c872d51bb01d523a2f3652ac
Yunki-H <yunki.dev@gmail.com>
2025-09-11 17:29:29 +0900

    [feat] #391 Semantic Color 추가

--- Hanbae/Extensions/ShapeStyle+Extension.swift
+++ Hanbae/Extensions/ShapeStyle+Extension.swift
@@ -9,65 +9,61 @@ import SwiftUICore
 
 extension ShapeStyle {
     static var bakBarGradient: LinearGradient {
-        .init(colors: [.top, .bottom], startPoint: .top, endPoint: .bottom)
+        .init(colors: [.bakBarTop, .bakBarBottom], startPoint: .top, endPoint: .bottom)
     }
 }
 
 extension ShapeStyle where Self == Color {
+    // MARK: - Brand
+    static var brandNormal: Color { .orange6 }
+    static var brandStrong: Color { .orange7 }
+    static var brandHeavy: Color { .orange8 }
+    
+    // MARK: - Theme
+    static var themeNormal: Color { .orange6 }
+    static var themeStrong: Color { .orange7 }
+    static var themeHeavy: Color { .orange8 }
+    
+    // MARK: - Label
+    static var labelDefault: Color { .neutral1 }
+    static var labelPrimary: Color { .common100 }
+    static var labelSecondary: Color { .neutral3 }
+    static var labelTertiary: Color { .neutral6 }
+    static var labelAssistive: Color { .neutral5 }
+    static var labelDisable: Color { .neutral4 }
+    static var labelInverse: Color { .common0 }
+    
     // MARK: - Background
-    static var backgroundDefault: Color { .black13 }
-    static var backgroundCard: Color { .black11 }
-    static var backgroundSheet: Color { .black12 }
-    static var backgroundNavigationBar: Color { .black11 }
-    static var backgroundImageDefault: Color { .black10 }
-    static var backgroundImageActive: Color { .orange400 }
-    static var backgroundPopupMenu: Color { .black10 }
-    static var backgroundBlur: Color { .black13 }
+    static var backgroundDefault: Color { .neutral13 }
+    static var backgroundElevated: Color { .neutral10 }
+    static var backgroundSubtle: Color { .neutral11 }
+    static var backgroundMute: Color { .neutral12 }
+    static var backgroundDark: Color { .common0 }
+    
+    // MARK: - Metronome
+    static var bakBarTop: Color { .redOrange6 }
+    static var bakBarBottom: Color { .orange6 }
+    static var bakBarInactive: Color { .neutral9 }
+    static var bakBarLine: Color { .common0 }
+    static var bakBarBorder: Color { .neutral8 }
+    static var bakBarNumberDefault: Color { .common100 }
+    static var bakBarNumberAlternative: Color { .neutral12 }
+    static var bakBarNumberInactive: Color { .neutral9 }
+    static var frame: Color { .neutral12 }
+    static var sobakSegmentDaebak: Color { .orange6 }
+    static var sobakSegmentSobak: Color { .orange4 }
+    static var blink: Color { .orange6 }
+    static var ornament: Color { .common100 }
+
+    // MARK: - Dimmer
+    static var dimmerDefault: Color { .common0.opacity(0.43) }
+    static var dimmerStrong: Color { .common0.opacity(0.74) }
+    static var dimmerHeavy: Color { .common0.opacity(0.88) }
     
     // MARK: - Button
-    static var buttonPrimary: Color { .black8 }
-    static var buttonBPMControlDefault: Color { .black10 }
-    static var buttonToggleOff: Color { .black9 }
-    static var buttonToggleOn: Color { .orange600 }
-    static var buttonToggleKnobInactive: Color { .black8 }
-    static var buttonCancel: Color { .black8 }
-    static var buttonPlayStart: Color { .black1 }
-    static var buttonPlayStop: Color { .black8 }
-    static var buttonActive: Color { .orange500 }
-    static var buttonBPMControlActive: Color { .black8 }
-    static var buttonReverse: Color { .black1 }
-    
-    // MARK: - BakDisplay
-    static var bakBarActiveBottom: Color { .bottom }
-    static var bakBarInactive: Color { .black9 }
-    static var frame: Color { .black11 }
-    static var bakBarLine: Color { .black13 }
-    static var bakBarNumberBlack: Color { .black11 }
-    static var bakBarNumberWhite: Color { .black1 }
-    static var bakBarBorder: Color { .black8 }
-    static var bakBarNumberGray: Color { .black9 }
-    static var bub: Color { .black3 }
-    static var sobakSegmentDaebak: Color { .orange500 }
-    static var sobakSegmentSobak: Color { .orange300 }
-    static var bakBarActiveTop: Color { .top }
-    static var bakBarIndicator: Color { .orange500 }
-    static var bakBarDivider: Color { .black8 }
-    
-    // MARK: - Text
-    static var textDefault: Color { .black1 }
-    static var textSecondary: Color { .black3 }
-    static var textTertiary: Color { .black6 }
-    static var textQuaternary: Color { .black7 }
-    static var textButtonPrimary: Color { .black1 }
-    static var textButtonSecondary: Color { .black2 }
-    static var textBPMDefault: Color { .black4 }
-    static var textBPMSearch: Color { .orange600 }
-    static var textButtonCancel: Color { .black6 }
-    static var textButtonEmphasis: Color { .black11 }
-    
-    // MARK: - ETC
-    static var progressPercentage: Color { .orange500 }
-    static var progressFrame: Color { .black8 }
-    static var jangdanLogoBackground: Color { .orange1300 }
-    static var jangdanLogoPrimary: Color { .orange700 }
+    static var buttonDefault: Color { .neutral8 }
+    static var buttonElevated: Color { .neutral7 }
+    static var buttonSubtle: Color { .neutral9 }
+    static var buttonMute: Color { .neutral10 }
+    static var buttonInverse: Color { .common100 }
 }
